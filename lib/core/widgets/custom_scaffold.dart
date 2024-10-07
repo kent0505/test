8d6d57dddc9b238935b180dfdbda9f50833d8928
kent0505 <otabekyusupov0550@gmail.com>
2024-10-07 13:02:00 +0500

    1

--- lib/core/widgets/custom_scaffold.dart
+++ lib/core/widgets/custom_scaffold.dart
@@ -11,35 +11,22 @@ class CustomScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Column(
+      body: Stack(
         children: [
-          // Container(
-          //   height: MediaQuery.of(context).viewPadding.top,
-          //   color: AppColors.white,
-          // ),
-          Expanded(
-            child: Stack(
-              children: [
-                // bg
-                Container(
-                  decoration: BoxDecoration(
-                    color: AppColors.black50,
-                    // gradient: LinearGradient(
-                    //   begin: Alignment.topCenter,
-                    //   end: Alignment.bottomCenter,
-                    //   colors: [
-                    //     Color(0xff072434),
-                    //     Color(0xff0D0819),
-                    //   ],
-                    // ),
-                  ),
-                ),
-
-                // content
-                body,
-              ],
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.black40,
+              // gradient: LinearGradient(
+              //   begin: Alignment.topCenter,
+              //   end: Alignment.bottomCenter,
+              //   colors: [
+              //     Color(0xff072434),
+              //     Color(0xff0D0819),
+              //   ],
+              // ),
             ),
           ),
+          body,
         ],
       ),
     );
